72c09c1e2417848293d583bc1faa5e1f342507dd
davidcoderistov <davidcoderistov@gmail.com>
2022-04-08 19:56:52 +0200

    Create colorable mixin

---
+++ src/mixins/colorable.ts
@@ -0,0 +1,41 @@
+import { VNodeData } from '../types'
+import { isCssColor } from '../util/colorUtils'
+
+export function setBackgroundColor(color?: string | false, data: VNodeData = {}): VNodeData {
+    if (isCssColor(color)) {
+        data.style = {
+            ...data.style as object,
+            'background-color': `${color}`,
+            'border-color': `${color}`,
+        }
+    } else if (color) {
+        data.class = {
+            ...data.class,
+            [color]: true,
+        }
+    }
+    return data
+}
+
+export function setTextColor (color?: string | false, data: VNodeData = {}): VNodeData {
+    if (isCssColor(color)) {
+        data.style = {
+            ...data.style as object,
+            color: `${color}`,
+            'caret-color': `${color}`,
+        }
+    } else if (color) {
+        const [colorName, colorModifier] = color.toString().trim().split(' ', 2) as (string | undefined)[]
+        data.class = {
+            ...data.class,
+            [colorName + '--text']: true,
+        }
+        if (colorModifier) {
+            data.class = {
+                ...data.class,
+                ['text--' + colorModifier]: true,
+            }
+        }
+    }
+    return data
+}
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+
+export type VNodeData = {
+    style?: object,
+    class?: object,
+}
