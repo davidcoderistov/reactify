ccaacbc79dbc4d447e86922db781fe1c048e52ec
davidcoderistov <davidcoderistov@gmail.com>
2022-04-08 20:06:00 +0200

    Create measurable mixin

---
+++ src/mixins/measurable.ts
@@ -0,0 +1,31 @@
+import { convertToUnit } from '../util/helpers'
+import { NumberOrNumberString } from '../types'
+
+type MeasurableStylesArgs = {
+    height: NumberOrNumberString,
+    maxHeight: NumberOrNumberString,
+    maxWidth: NumberOrNumberString,
+    minHeight: NumberOrNumberString,
+    minWidth: NumberOrNumberString,
+    width: NumberOrNumberString,
+}
+
+export function measurableStyles(measurableStylesArg: MeasurableStylesArgs): object {
+    const styles: Record<string, string> = {}
+
+    const height = convertToUnit(measurableStylesArg.height)
+    const minHeight = convertToUnit(measurableStylesArg.minHeight)
+    const minWidth = convertToUnit(measurableStylesArg.minWidth)
+    const maxHeight = convertToUnit(measurableStylesArg.maxHeight)
+    const maxWidth = convertToUnit(measurableStylesArg.maxWidth)
+    const width = convertToUnit(measurableStylesArg.width)
+
+    if (height) styles.height = height
+    if (minHeight) styles.minHeight = minHeight
+    if (minWidth) styles.minWidth = minWidth
+    if (maxHeight) styles.maxHeight = maxHeight
+    if (maxWidth) styles.maxWidth = maxWidth
+    if (width) styles.width = width
+
+    return styles
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,5 @@ export type VNodeData = {
     style?: object,
     class?: object,
 }
+
+export type NumberOrNumberString = string | number | undefined
